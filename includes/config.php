7244503596b487b9676ab85c9498e8d6f80d6cc4
root <root@ip-172-31-33-162.ap-south-1.compute.internal>
2021-02-26 06:45:38 +0000

    asdf

--- includes/config.php
+++ includes/config.php
@@ -4,13 +4,13 @@
 	//----------------------------------------------------------------------------------//
 	
 	/*  Set the URL to your Sendy installation (without the trailing slash) */
-	define('APP_PATH', 'http://your_sendy_installation_url');
+	define('APP_PATH', 'https://codegnan.com/sendy');
 	
 	/*  MySQL database connection credentials (please place values between the apostrophes) */
-	$dbHost = ''; //MySQL Hostname
-	$dbUser = ''; //MySQL Username
-	$dbPass = ''; //MySQL Password
-	$dbName = ''; //MySQL Database Name
+	$dbHost = 'localhost'; //MySQL Hostname
+	$dbUser = 'sendy'; //MySQL Username
+	$dbPass = 'qwerty'; //MySQL Password
+	$dbName = 'sendy'; //MySQL Database Name
 	
 	
 	//----------------------------------------------------------------------------------//	
